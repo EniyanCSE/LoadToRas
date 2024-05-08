2d11cf2a79dd7595da97f363820aad3177ec6d3a
Eniyan <112230852+EniyanCSE@users.noreply.github.com>
2024-05-08 21:34:32 +0530

    facedetection.py

--- facedetection.py
+++ facedetection.py
@@ -1,41 +1,34 @@
 import cv2
 import numpy as np
-import time
 
-# pengklasifikasiWajah  = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-
-haarcascade_path = 'E:\Amrita\Sem 6\Robo\Project\Face-Detection\haarcascade_frontalface_default.xml'
+# Ensure the haarcascade path is correct and file is available on Raspberry Pi
+haarcascade_path = '/home/pi/haarcascade_frontalface_default.xml'  # Adjust path as needed
 pengklasifikasiWajah = cv2.CascadeClassifier(haarcascade_path)
-
 
 videoCam = cv2.VideoCapture(0)
 
 if not videoCam.isOpened():
-    print("Targets Detected")
+    print("Could not open video camera")
     exit()
 
 tombolQditekan = False
-while (tombolQditekan == False):
+while not tombolQditekan:
     ret, kerangka = videoCam.read()
 
-    if ret == True:
+    if ret:
         abuAbu = cv2.cvtColor(kerangka, cv2.COLOR_BGR2GRAY)
-        dafWajah = pengklasifikasiWajah.detectMultiScale(abuAbu, scaleFactor = 1.3, minNeighbors = 2)
+        dafWajah = pengklasifikasiWajah.detectMultiScale(abuAbu, scaleFactor=1.3, minNeighbors=2)
 
         for (x, y, w, h) in dafWajah:
             cv2.rectangle(kerangka, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        
-        #print("Jumlah Wajah terdeksi: ", len(dafWajah))
-        teks = "Jumlah Wajah Terdeteksi = " + str(len(dafWajah))
 
+        teks = "Jumlah Wajah Terdeteksi = " + str(len(dafWajah))
         font = cv2.FONT_HERSHEY_SIMPLEX
         cv2.putText(kerangka, teks, (0, 30), font, 1, (255, 0, 0), 1)
-
         cv2.imshow("Hasil", kerangka)
+        
         if cv2.waitKey(1) & 0xFF == ord('q'):
             tombolQditekan = True
-            break
-
 
 videoCam.release()
 cv2.destroyAllWindows()
